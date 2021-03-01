5c9f7cf0fa4188f4ee7555ced094ae86c28611aa
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-01 11:19:40 +0600

    Update Template Literals.js

--- 10 Days of Javascript/Day 5/Template Literals.js
+++ 10 Days of Javascript/Day 5/Template Literals.js
@@ -1,12 +1,3 @@
-/*
- * Determine the original side lengths and return an array:
- * - The first element is the length of the shorter side
- * - The second element is the length of the longer side
- * 
- * Parameter(s):
- * literals: The tagged template literal's array of strings.
- * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
- */
 function sides(literals, ...expressions) {
     var area = expressions[0];
     var perimeter = expressions[1];
