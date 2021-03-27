c80e595f21a2400ebf65c72e5485d18d71a40d43
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-27 11:55:36 +0600

    Update test.c

--- test.c
+++ test.c
@@ -22,3 +22,15 @@ int main() {
    
     return 0;
 }
+//Pointer
+#include <stdio.h>
+
+int main() {
+   double pi=3.141592;
+   int *ptr;
+   ptr=&pi;
+   printf("value of pi : %lf\n",pi);
+   printf("value of pi : %lf\n",*ptr);
+   
+    return 0;
+}
