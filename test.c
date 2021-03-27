a8ec1f12e66105600f93e3a21e64f167046c2ae8
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-27 11:49:04 +0600

    Update test.c

--- test.c
+++ test.c
@@ -9,3 +9,16 @@ int main() {
    }
     return 0;
 }
+
+//capital_letter
+#include <stdio.h>
+
+int main() {
+   char small_letter,capital_letter;
+   printf("Please enter a small letter :");
+   small_letter=getchar();
+   capital_letter=small_letter - 32;
+   printf("The capital letter is : %c\n",capital_letter);
+   
+    return 0;
+}
